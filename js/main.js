68393b8a31c08de36e6ff8f4af4b3b41dee2bd1e
Nitram <144196861+NitramIast@users.noreply.github.com>
2023-10-23 16:26:38 +0800

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,77 @@
+const academy = [
+    {
+            name: '文學院',
+            img: '../img/btn-images.jpg',
+            info: ['人文大樓'],
+        },
+        {
+            name: '農業暨自然資源學院',
+            img: '../img/btn-images.jpg',
+            info: ['作物科學大樓', '作物科學大樓2樓', '森林館', '應經一館', '農環大樓',
+                '動科系館', '水土保持館', '食品暨生物科技大樓', '生機大樓', '生管所', '食安大樓',
+                '綜合教學大樓', '國農大樓', '舊理工大樓', '國農中心', '應用經濟學系一館']
+        },
+        {
+            name: '理學院',
+            img: '../img/btn-images.jpg',
+            info: ['化學館', '資訊科學大樓', '理學大樓', '資訊科學大樓']
+        },
+        {
+            name: '工學院',
+            img: '../img/btn-images.jpg',
+            info: ['土木環工大樓', '混擬土科技研究中心', '應用科技大樓', '機械系館'
+                , '化材大樓', '應用科技大樓']
+        },
+        {
+            name: '生命科學院',
+            img: '../img/btn-images.jpg',
+            info: ['生命科學大樓', '防疫檢疫大樓']
+        },
+        {
+            name: '獸醫學院',
+            img: '../img/btn-images.jpg',
+            info: ['獸醫系館', '動物疾病診斷中心']
+        },
+        {
+            name: '管理學院',
+            img: '../img/btn-images.jpg',
+            info: ['社管大樓', '綜合教學大樓']
+        },
+        {
+            name: '法政學院',
+            img: '../img/btn-images.jpg',
+            info: ['社管大樓']
+        },
+        {
+            name: '電機資訊學院',
+            img: '../img/btn-images.jpg',
+            info: ['電機大樓', '理學大樓']
+        },
+        {
+            name: '醫學院',
+            img: '../img/btn-images.jpg',
+            info: ['國農中心', '生命科學大樓', '防疫檢疫大樓']
+        },
+        {
+            name: '創新產業暨國際學院',
+            img: '../img/btn-images.jpg',
+            info: ['綜合教學大樓', '國農中心']
+        },
+        {
+            name: '行政',
+            img: '../img/btn-images.jpg',
+        },
+        {
+            name: '圖書',
+            img: '../img/btn-images.jpg',
+        },
+        {
+            name: '體育',
+            img: '../img/btn-images.jpg',
+        },
+        {
+            name: '餐廳',
+            img: '../img/btn-images.jpg',
+        },
+
+    ]
