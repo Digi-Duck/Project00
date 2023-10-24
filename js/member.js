73bacdfc0573be69e2fd98cf3b1eaeb8d38f1312
Nitram <144196861+NitramIast@users.noreply.github.com>
2023-10-24 23:34:53 +0800

    真的做完了

---
+++ js/member.js
@@ -0,0 +1,32 @@
+const groupMember = Object.freeze(
+    [
+        {
+            name: '黃絜琳',
+            position: '組長/程式',
+            image: '../img/04.png',
+        },
+        {
+            name: '蔡明哲',
+            position: '程式',
+            image: '../img/04.png',
+        },
+        {
+            name: '陳映達',
+            position: '企劃',
+            image: '../img/04.png',
+        },
+        {
+            name: '吳睿燊',
+            position: '設計',
+            image: '../img/04.png',
+        },
+        {
+            name: '賴菱俐',
+            position: '設計',
+            image: '../img/04.png',
+        },
+    ]
+);
+
+
+export { groupMember };
