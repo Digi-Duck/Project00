acf5d300048e43ce499182aa8bea4254334c4449
Nitram <Nitram.Iast0508@gmail.com>
2023-11-02 01:00:03 +0800

    組員照片

--- js/member.js
+++ js/member.js
@@ -3,27 +3,27 @@ const groupMember = Object.freeze(
         {
             name: '黃絜琳',
             position: '組長/程式',
-            image: '../img/04.png',
+            image: './img/member2.jpg',
         },
         {
             name: '蔡明哲',
             position: '程式',
-            image: '../img/04.png',
+            image: './img/member5.jpg',
         },
         {
             name: '陳映達',
             position: '企劃',
-            image: '../img/04.png',
+            image: './img/member4.jpg',
         },
         {
             name: '吳睿燊',
             position: '設計',
-            image: '../img/04.png',
+            image: './img/member3.jpg',
         },
         {
             name: '賴菱俐',
             position: '設計',
-            image: '../img/04.png',
+            image: './img/member1.jpg',
         },
     ]
 );
